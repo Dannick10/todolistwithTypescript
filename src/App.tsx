88292252f9cd20eb0aa9f5fa9301442275494d06
unknown <danmaggot2@gmail.com>
2024-06-10 15:23:06 -0300

    footer & header

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
 import { useState } from 'react'
 import './App.css'
 
+import Header from './components/Header'
+import Footer from './components/Footer'
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    
+      <div>
+      <Header />
+
+        <h1>Conteudo</h1>
+      <Footer />
+
+      </div>    
     </>
   )
 }
---
+++ src/components/Footer.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+type Props = {}
+
+const Footer = (props: Props) => {
+  return (
+    <footer>
+    <p>
+      <span>React + ts todo</span> @2024
+    </p>
+  </footer>
+  )
+}
+
+export default Footer
