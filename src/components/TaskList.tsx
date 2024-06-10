e967b090eb78ecf42aa5cccd9e75b3d0f4bbabc9
unknown <danmaggot2@gmail.com>
2024-06-10 18:01:57 -0300

    tarefa styles

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 import { Itask } from '../interfaces/Task'
 
-import styles from '../interface/taskList.module.css'
+import styles from './taskList.module.css'
 
 interface Props {
   taskListArray: Itask[]
@@ -13,12 +13,12 @@ const TaskList = ({taskListArray}: Props) => {
     <>  
   {taskListArray.length > 0 ? (
       taskListArray.map((test) => (
-        <div key={test.id}>
-          <div>
+        <div key={test.id} className={styles.task}>
+          <div className={styles.details}>
           <h2>{test.title}</h2>
           <p>Dificuldade: {test.difficulty}</p>
           </div>
-          <div>
+          <div className={styles.actions}>
           <i className="fa-solid fa-pencil"></i>
           <i className="fa-solid fa-trash"></i>
           </div>
